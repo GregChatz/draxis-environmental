573995ea91892bc063f58750cb64a054f15cd089
GregChatz <48617279+GregChatz@users.noreply.github.com>
2022-09-05 23:12:25 +0300

    Add files via upload

---
+++ draxis-weather-widget/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "b06df9aad8msh32ed68b6e15a328p1f885cjsn782b848a1fb7",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "a0163c0af18f29ef334ff3a6ad153f69";
+  
